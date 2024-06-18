afa7129ccc23184817ec122aa55cd0dc700e4a26
mGoicoecheaFiuba <mgoicoechea@fi.uba.ar>
2024-06-17 22:55:41 -0300

    Se realizan cambios en el models.py: -Se agrega una nueva tabla peliculas_plataforma la cual relaciona ambas, esto soluciona el problema de que haya una pelicula en varias plataformas -Se modifica la columna plataforma_id. La cambiamos por plataformas la cual recibira una lista de ids de las plataformas en las que se encuentra la pelicula

--- backend/models.py
+++ backend/models.py
@@ -1,27 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db= SQLAlchemy()
+db = SQLAlchemy()
+
+# tabla intermedia entre peliculas y plataformas
+peliculas_plataformas = db.Table('peliculas_plataformas',
+    db.Column('pelicula_id', db.Integer, db.ForeignKey('peliculas.id'), primary_key=True),
+    db.Column('plataforma_id', db.Integer, db.ForeignKey('plataformas.id'), primary_key=True)
+)
 
 class Pelicula(db.Model):
-    __tablename__= 'peliculas'
-    id= db.Column(db.Integer, primary_key=True)
-    titulo= db.Column(db.String(255), nullable= False)
-    sinopsis= db.Column(db.String(255), nullable= False)
-    estreno= db.Column(db.String(255), nullable=False)
-    director= db.Column(db.String(255), nullable=False)
-    genero= db.Column(db.String(255), nullable=False)
-    plataforma_id= db.Column(db.Integer, db.ForeignKey('plataformas.id'))
-    imagen= db.Column(db.String(255))
+    __tablename__ = 'peliculas'
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(255), nullable=False)
+    sinopsis = db.Column(db.String(255), nullable=False)
+    estreno = db.Column(db.String(255), nullable=False)
+    director = db.Column(db.String(255), nullable=False)
+    genero = db.Column(db.String(255), nullable=False)
+    # relacion con plataformas
+    plataformas = db.relationship('Plataforma', secondary=peliculas_plataformas, backref=db.backref('peliculas', lazy='dynamic'))
+    imagen = db.Column(db.String(255))
 
 class Plataforma(db.Model):
-    __tablename__= 'plataformas'
-    id= db.Column(db.Integer, primary_key=True)
-    nombre= db.Column(db.String(255), nullable= False)
-    imagen= db.Column(db.String(255))
+    __tablename__ = 'plataformas'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    imagen = db.Column(db.String(255))
 
 class Opinion(db.Model):
-    __tablename__= 'opiniones'
-    id= db.Column(db.Integer, primary_key=True)
-    pelicula_id= db.Column(db.Integer, db.ForeignKey('peliculas.id'))
-    valoracion= db.Column(db.Integer)
-    comentario= db.Column(db.String(255), nullable= False)
+    __tablename__ = 'opiniones'
+    id = db.Column(db.Integer, primary_key=True)
+    pelicula_id = db.Column(db.Integer, db.ForeignKey('peliculas.id'))
+    valoracion = db.Column(db.Integer)
+    comentario = db.Column(db.String(255), nullable=False)
