cb972f2c0181777baa17788c5e693777980be6fe
guido <gromerok@fi.uba.ar>
2024-06-17 22:28:10 -0300

    se permiten CORS

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request
 from models import db, Pelicula, Plataforma, Opinion
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app, supports_credentials=True)
+
 port = 5000
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://joaco:joaco2005@localhost:5432/peliculas_web'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://guido:guido@localhost:5432/peliculas'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
